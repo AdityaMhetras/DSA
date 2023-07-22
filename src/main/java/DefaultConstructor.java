c3a43accc03c1fe5b628599cabca243aef4ac010
Aditya <mhetrasaditya@gmail.com>
2023-07-22 11:54:52 +0530

    Default constructor

---
+++ src/main/java/DefaultConstructor.java
@@ -0,0 +1,13 @@
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class DefaultConstructor {
+
+    String a;
+
+
+    public static void main(String[] args) {
+        DefaultConstructor defaultConstructor = new DefaultConstructor();
+
+    }
+}
